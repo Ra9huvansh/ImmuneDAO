7dd57c59a24dea7a980175530ebe1bd6e12ea1aa
Raghuvansh <raghuvansh.siddharth004@gmail.com>
2025-10-03 00:16:13 +0530

    updated with comments and versions

--- src/TimeLock.sol
+++ src/TimeLock.sol
@@ -5,6 +5,10 @@ pragma solidity ^0.8.18;
 import {TimelockController} from "@openzeppelin/contracts/governance/TimelockController.sol";
 
 contract TimeLock is TimelockController {
+
+    // minDelay is how long you have to wait before executing a proposal
+    // proposers is the list of addresses that can propose
+    // executors is the list of addresses that can execute
     constructor(
         uint256 minDelay,
         address[] memory proposers,
